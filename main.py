6c4c4181c364e8b66ffa0b0e528da1c8863542a2
Thomas Bond <tcoolb007@gmail.com>
2016-11-29 01:44:00 -0500

    Use HAT's get_event() method instead of pygame.

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import pygame
-
-from pygame.locals import *
-from sense_hat import SenseHat
+import sense_hat
 from time import sleep
 
-sense = SenseHat()
-pygame.init()
-pygame.display.set_mode((640, 480))
+sense = sense_hat.SenseHat()
 
 sense.set_rotation(180)
 
@@ -19,6 +14,8 @@ e = [0, 0, 0]
 man_x = 1
 man_y = 4
 blacklist = [[0,5],[1,5],[2,4],[3,4],[4,5],[5,5],[6,5],[7,5]]
+pressed = sense_hat.ACTION_PRESSED
+
 
 go = [
 e,e,e,e,e,e,e,e,
@@ -65,23 +62,23 @@ def whitelist(x,y):
     return True
 
 while running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_UP and man_y < 7 and whitelist(man_x, man_y + 1):
+    for event in sense.stick.get_events():
+        if event.action == pressed:
+            if event.direction == sense_hat.DIRECTION_UP and man_y < 7 and whitelist(man_x, man_y + 1):
                 man_y = man_y + 1
-            if event.key == K_DOWN and man_y > 0 and whitelist(man_x, man_y - 1):
+            if event.direction == sense_hat.DIRECTION_DOWN and man_y > 0 and whitelist(man_x, man_y - 1):
                 man_y = man_y - 1
-            if event.key == K_LEFT and man_x < 7 and whitelist(man_x + 1, man_y):
+            if event.direction == sense_hat.DIRECTION_LEFT and man_x < 7 and whitelist(man_x + 1, man_y):
                 man_x = man_x + 1
-            if event.key == K_RIGHT and man_x > 0 and whitelist(man_x - 1, man_y):
+            if event.direction == sense_hat.DIRECTION_RIGHT and man_x > 0 and whitelist(man_x - 1, man_y):
                 man_x = man_x - 1
             sense.set_pixels(bg)
             sense.set_pixel(man_x, man_y, [255, 125, 0])
-        if event.type == QUIT:
-            running = False
+#        if event.type == QUIT:
+#            running = False
     if man_x == 7:
         sense.set_pixels(win)
-        print("you win")
+        print("\nyou win")
         sleep(10)
         running = False
 
